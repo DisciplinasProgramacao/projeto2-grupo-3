3fc3312ae1ca9144ecabb2e4a6aba277633d55a9
Lucas Santos <lucassr614@gmail.com>
2022-09-18 19:51:24 -0300

    Update testes classe Aresta

---
+++ codigo/Grafo/src/ArestaTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArestaTest {
+
+    static Aresta aresta;
+
+    @BeforeAll
+    public static void criaAresta() {
+        aresta = new Aresta(2, 3);
+    }
+
+    @Test
+    public void criarArestaCorreta() {
+        assertEquals(2, aresta.getPeso());
+        assertEquals(3, aresta.getDestino());
+    }
+
+    @Test
+    public void visitaCorreta() {
+        aresta.visitar();
+        assertTrue(aresta.isVisitada());
+    }
+
+    @Test
+    public void limpaVisita() {
+        aresta.visitar();
+        aresta.limparVisita();
+        assertFalse(aresta.isVisitada());
+    }
+
+
+}

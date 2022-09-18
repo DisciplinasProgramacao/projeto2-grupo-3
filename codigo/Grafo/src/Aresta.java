e8e0710f8d4f5f1eeb7c04b2007c5fd654e8284a
Lucas Santos <lucassr614@gmail.com>
2022-09-18 19:51:24 -0300

    Update testes classe Aresta #3

--- codigo/Grafo/src/Aresta.java
+++ codigo/Grafo/src/Aresta.java
@@ -1,18 +1,18 @@
-/** 
+/**
  * MIT License
- *
+ * <p>
  * Copyright(c) 2021 João Caram <caram@pucminas.br>
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,49 +24,47 @@
 
 public class Aresta {
 
+    //#region Atributos
     private int peso;
     private int destino;
     private boolean visitada;
+//#endregion
 
     /**
      * Construtor para arestas com ou sem peso
-     * @param peso Peso da aresta
-     * @param dest Vértice de destino
+     *
+     * @param peso    Peso da aresta
+     * @param destino Vértice de destino
      */
-    public Aresta(int peso, int destino){
+    public Aresta(int peso, int destino) {
         this.peso = peso;
         this.destino = destino;
         this.visitada = false;
     }
-
-    /**
-     * Método de acesso para o peso da aresta
-     * @return the peso
-     */
-    public int peso() {
-        return this.peso;
-    }
-   
-    /**
-     * Método de acesso para o destino da aresta
-     * @return the destino
-     */
-    public int destino() {
-        return this.destino;
-    }
-
-    public void visitar(){
+    //#region Métodos públicos
+    public void visitar() {
         this.visitada = true;
     }
-
-    public void limparVisita(){
+    public void limparVisita() {
         this.visitada = false;
     }
 
-    public boolean visitada(){
+    //#endregion
+    //#region Getters and Setters
+    /**
+     * Método de acesso para o destino da aresta
+     *
+     * @return the destino
+     */
+    public int getDestino() {return this.destino;}
+    /**
+     * Método de acesso para o peso da aresta
+     *
+     * @return the peso
+     */
+    public int getPeso() {return this.peso;}
+    public boolean isVisitada() {
         return this.visitada;
     }
-
-    
-
+    //#endregion
 }

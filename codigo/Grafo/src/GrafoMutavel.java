1b4834a017cc99c3b49aed97e376ed4d58b2edca
THiago Soares Mota <thiagomota456@gmail.com>
2022-09-27 06:42:37 -0300

    Classe GrafoMutavel finalizado #10.

--- codigo/Grafo/src/GrafoMutavel.java
+++ codigo/Grafo/src/GrafoMutavel.java
@@ -1,5 +1,4 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -7,12 +6,17 @@ import java.io.ObjectOutputStream;
 
 public abstract class GrafoMutavel extends Grafo {
 
+    /**
+     * Construtor. Cria um grafo vazio com capacidade para MAX_VERTICES
+     * @param nome  nome do grafo
+     */
     public GrafoMutavel(String nome) {
         super(nome);
     }
 
     /**
      * Adiciona, se possível, um vértice ao grafo. O vértice é auto-nomeado com o próximo id disponível.
+     * @return TRUE caso add a vertice, e FALSE caso não add
      */
     public boolean addVertice(int id) {
         Vertice novo = new Vertice(id);
@@ -25,11 +29,15 @@ public abstract class GrafoMutavel extends Grafo {
      *
      * @param origem  Vértice de origem
      * @param destino Vértice de destino
+     * @return TRUE caso add a aresta, FALSE caso não add
      */
     public boolean addAresta(int origem, int destino) {
+
         boolean adicionou = false;
+
         Vertice saida = this.existeVertice(origem);
         Vertice chegada = this.existeVertice(destino);
+
         if (saida != null && chegada != null) {
             saida.addAresta(destino);
             chegada.addAresta(origem);
@@ -37,9 +45,13 @@ public abstract class GrafoMutavel extends Grafo {
         }
 
         return adicionou;
-
     }
 
+    /**
+     *Salva um arquivo que representa um grafo
+     * 
+     * @param nomeArquivo  nome do arquivo a ser salvo
+     */
     public void salvar(String nomeArquivo) throws IOException {
         FileOutputStream fOut = new FileOutputStream(nomeArquivo);
         ObjectOutputStream oOut = new ObjectOutputStream(fOut);
@@ -47,45 +59,44 @@ public abstract class GrafoMutavel extends Grafo {
         oOut.close();
     }
 
-    public GrafoMutavel carregar(String nomeArquivo) throws IOException, ClassNotFoundException {
-        FileInputStream fOut = new FileInputStream(nomeArquivo);
-        ObjectInputStream oOut = new ObjectInputStream(fOut);
+    /**
+     * Lê um arquivo que representa um grafo
+     * 
+     * @param nomeArquivo  nome do arquivo a ser lido
+     */
+    public void carregar(String nomeArquivo) throws IOException, ClassNotFoundException{
+        FileInputStream fOut= new FileInputStream(nomeArquivo);
+        ObjectInputStream oOut= new ObjectInputStream(fOut);
         GrafoMutavel g = (GrafoMutavel) oOut.readObject();
+        this.vertices = g.vertices;
         oOut.close();
-
-        return g;
     }
 
+    /**
+     * Deleta arquivo a partir de origem e destiino
+     * 
+     * @param origem int que representa a origem da aresta
+     * @param destino int que representa o destino da arest
+     * @return retorna TRUE caso a exclusão seja bem sucedida e FALSE caso contrario
+     */
     public boolean delAresta(int origem, int destino) {
-
-        //Aqui considerei que pode haver um loop. Assim uma aresta vai estar ligada a um unico vertice. 
-        //Logo a origem é o destino serão um só. Sendo assim as o return vai ser necessáriamente True e False
-        //Não estou seguro que essa lógica esta certa, e estou com sono kkkkk. Logo fiquem  a vontade se quiserem corrigir antes de mim 
 
         Vertice[] verticeArray = new Vertice[vertices.size()];
         verticeArray = vertices.allElements(verticeArray);
 
-        var destinoRemovido = false;
-        var origemRemovido = false;
+        Vertice orige = this.vertices.get(origem);
+        Vertice dest = this.vertices.get(destino);
 
-        for (var v : verticeArray) {
-            //Acho q isso vai ter que mudar se a gente resolver implementar grafos direcionais
-            if (v.delAresta(destino)) {
-                destinoRemovido = true;
-            }
-            if (v.delAresta(origem)) {
-                origemRemovido = true;
-            }
-        }
-
-        return destinoRemovido || origemRemovido;
+        return orige.delAresta(origem) && dest.delAresta(destino);
     }
 
-    public void escreveGrafoArquivo(Grafo grafo) {
-
-    }
-
-
+    /**
+     * Deleta arquivo a partir de origem e destiino
+     * 
+     * @param origem int que representa a origem da aresta
+     * @param destino int que representa o destino da arest
+     * @return retorna TRUE caso a exclusão seja bem sucedida e FALSE caso contrario
+     */
     public boolean delVertice(int idVertice) {
 
         //Acho q isso vai ter que mudar se a gente resolver implementar grafos direcionais
